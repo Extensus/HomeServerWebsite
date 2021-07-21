949c1ba145da06486254fadfbca5099777a698d8
Eric Stautmeister <eric@stautmeister.ch>
2021-07-21 16:41:13 +0200

    visual improvements

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,7 +1,13 @@
 const grid = document.querySelector('#hex-grid');
 const light = document.querySelector('.light')
+const actionContainer = document.querySelector('.action-container')
 
 grid.addEventListener('mousemove', function(e) {
+light.style.left = `${e.clientX}px`;
+light.style.top = `${e.clientY}px`;
+});
+
+actionContainer.addEventListener('mousemove', function(e) {
 light.style.left = `${e.clientX}px`;
 light.style.top = `${e.clientY}px`;
 });
