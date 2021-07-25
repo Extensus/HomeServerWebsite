0774008e0fbe8ab06424b88109f85cc483ef8f9f
Eric Stautmeister <eric@stautmeister.ch>
2021-07-25 13:50:45 +0200

    -

--- HTML/js/filedrop.js
+++ HTML/js/filedrop.js
@@ -32,64 +32,69 @@ function getFilesWebkitDataTransferItems(dataTransferItems) {
     })
 }
 
+function dropZoneHandler(item) {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", (e) => { //Listener
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", (e) => { //Listener
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", (e) => { //Listener
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach((type) => { //Drag-Drop Handler
+        dropZoneElement.addEventListener(type, (e) => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        });
+    });
+
+    dropArea.addEventListener("drop", (e) => { //OnDrop Action Handler
+        e.preventDefault();
+
+        var items = e.dataTransfer.items;
+        getFilesFromWebkitDataTransferItems(items)
+            .then(files => {
+                {}
+            });
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+    }, false);
+
+    /**
+     * Updates the thumbnail on a drop zone element.
+     *
+     * @param {HTMLElement} dropZoneElement
+     * @param {File} file
+     */
+
+    let uploadedFiles = [];
+
+    function updateThumbnail(dropZoneElement, file) {
+        let dropPrompt = document.getElementById('temp-text');
+        let elementUploaded = document.getElementById('uploadedFiles');
+
+        if (!elementUploaded) {
+            dropPrompt.remove();
+            uploadedFiles.forEach((item) => {
+                let li = document.createElement('li');
+                li.innerText = item;
+                elementUploaded.appendChild(li);
+            });
+        }
+    }
+}
+
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
-            const dropZoneElement = inputElement.closest(".drop-zone");
-
-            dropZoneElement.addEventListener("click", (e) => { //Listener
-                inputElement.click();
-            });
-
-            inputElement.addEventListener("change", (e) => { //Listener
-                if (inputElement.files.length) {
-                    updateThumbnail(dropZoneElement, inputElement.files[0]);
-                }
-            });
-
-            dropZoneElement.addEventListener("dragover", (e) => { //Listener
-                e.preventDefault();
-                dropZoneElement.classList.add("drop-zone--over");
-            });
-
-            ["dragleave", "dragend"].forEach((type) => { //Drag-Drop Handler
-                dropZoneElement.addEventListener(type, (e) => {
-                    dropZoneElement.classList.remove("drop-zone--over");
-                });
-            });
-
-            dropArea.addEventListener("drop", (e) => { //OnDrop Action Handler
-                e.preventDefault();
-
-                var items = e.dataTransfer.items;
-                getFilesFromWebkitDataTransferItems(items)
-                    .then(files => {
-                        {}
-                    });
-                if (e.dataTransfer.files.length) {
-                    inputElement.files = e.dataTransfer.files;
-                    updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-                }
-
-            }, false);
-
-            /**
-             * Updates the thumbnail on a drop zone element.
-             *
-             * @param {HTMLElement} dropZoneElement
-             * @param {File} file
-             */
-
-            let uploadedFiles = [];
-
-            function updateThumbnail(dropZoneElement, file) {
-                let dropPrompt = document.getElementById('temp-text');
-                let elementUploaded = document.getElementById('uploadedFiles');
-
-                if (!elementUploaded) {
-                    dropPrompt.remove();
-                    uploadedFiles.forEach((item) => {
-                        let li = document.createElement('li');
-                        li.innerText = item;
-                        elementUploaded.appendChild(li);
-                    });
-                }
-            }
+    dropZoneHandler(inputElement)
+})
