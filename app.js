5173de771f77edd855d2dc1d367c4fa308818232
Eric Stautmeister <eric@stautmeister.ch>
2021-08-08 19:18:48 +0200

    ejs renderer implemented

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('HTML'))
+app.use('/css', express.static(`${__dirname}/HTML/css`))
+app.use('/form', express.static(`${__dirname}/HTML/form`))
+app.use('/submit', express.static(`${__dirname}/HTML/form/submit`))
+app.use('/images', express.static(`${__dirname}/HTML/images`))
+app.use('/js', express.static(`${__dirname}/HTML/js`))
+app.use('/pages', express.static(`${__dirname}/HTML/pages`))
+app.use('/uploads', express.static(`${__dirname}/HTML/uploads`))
+
+app.set('views', './HTML/pages')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render(`text`)
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render(`dashboard`)
+});
+
+app.get('/filedrop', (req, res) => {
+    res.render(`filedrop`)
+});
+
+app.get('/notepad', (req, res) => {
+    res.render(`notepad`)
+});
+
+app.listen(port, () => console.info(`App available on http://localhost:${port}`))
