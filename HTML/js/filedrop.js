0477fae5a6fe6beb5c7d0b4382cb6624a55440c8
Eric Stautmeister <eric@stautmeister.ch>
2021-08-08 18:28:04 +0200

    -

--- HTML/js/filedrop.js
+++ HTML/js/filedrop.js
@@ -10,6 +10,15 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
         {}
       }
     });
+
+    dropZoneElement.addEventListener("mouseover", (e) => {
+      e.preventDefault();
+      dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    dropZoneElement.addEventListener("mouseout", (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
   
     dropZoneElement.addEventListener("dragover", (e) => {
       e.preventDefault();
@@ -24,12 +33,6 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   
     dropZoneElement.addEventListener("drop", (e) => {
       e.preventDefault();
-  
-      // if (e.dataTransfer.files.length) {
-      //   inputElement.files = e.dataTransfer.files;
-      //   updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-      // }
-  
       dropZoneElement.classList.remove("drop-zone--over");
     });
   });
